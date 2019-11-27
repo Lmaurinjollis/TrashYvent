53cd1c9cb0440e5ecbe8e50730808a1c5e124332
Ludwig Maurin-Jollis--Gally <ludwig.maurinjollisgally@viacesi.fr>
2019-11-27 10:35:51 +0100

    Homepage Map
    Google Map integrated with a dev API Key.

--- src/app/homepage/homepage.page.ts
+++ src/app/homepage/homepage.page.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapOptions,
+  GoogleMapsEvent,
+  Marker,
+  HtmlInfoWindow
+} from '@ionic-native/google-maps';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Platform } from '@ionic/angular';
+
+declare var google;
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +19,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepagePage implements OnInit {
 
-  constructor() { }
+  @ViewChild('map', null) mapElement: ElementRef;
+  map: GoogleMap;
+  address: string;
 
-  ngOnInit() {
+  constructor(private platform: Platform, private geolocation: Geolocation) { }
+
+  async ngOnInit() {
+
+    await this.platform.ready();
+    await this.loadMap();
+  }
+
+  loadMap() {
+    this.map = GoogleMaps.create('map_canvas', {
+      camera: {
+        target: {
+          lat: 43.548443,
+          lng: 1.502883
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    });
+  }
+
+  recenterMap() {
+    let lat = 43.548443;
+    let lng = 1.502883;
+    this.geolocation.getCurrentPosition().then((resp) => {
+      lat = resp.coords.latitude;
+      lng = resp.coords.longitude;
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+
+    this.map.setCameraTarget({lat, lng});
   }
 
 }
